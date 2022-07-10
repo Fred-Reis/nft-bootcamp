a79d2845537e02de462034baacb9ac99d28c9f6a
Fred-Reis <fredgost@hotmail.com>
2022-07-09 22:31:41 -0300

    add function to create aleatory colors

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -12,8 +12,27 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokensIds;
 
-    string baseSvg =
-        "<svg  xmlns='http://www.w3.org/2000/svg'  preserveAspectRatio='xMinYMin meet'  viewBox='0 0 350 350'>  <defs>    <linearGradient id='Gradient1'>      <stop class='stop1' offset='0%'/>      <stop class='stop2' offset='50%'/>      <stop class='stop3' offset='100%'/>    </linearGradient>  </defs>  <style>    .base {      fill: blue;      font-family: serif;      font-size: 20px;      color: #FFF;    }    .stop1 { stop-color: green; }    .stop2 { stop-color: white; stop-opacity: 0; }    .stop3 { stop-color: yellow; }      </style>  <rect width='100%' height='100%' fill='url(#Gradient1)' />  <text    x='50%'    y='50%'    class='base'    dominant-baseline='middle'    text-anchor='middle'  >";
+    string svgPartOne =
+        "<svg  xmlns='http://www.w3.org/2000/svg'  preserveAspectRatio='xMinYMin meet'  viewBox='0 0 350 350'>  <defs>    <linearGradient id='Gradient1'>      <stop class='stop1' offset='0%'/>      <stop class='stop2' offset='50%'/>      <stop class='stop3' offset='100%'/>    </linearGradient>  </defs>  <style>    .base {      fill: blue;      font-family: serif;      font-size: 20px;      color: #FFF;    }    .stop1 { stop-color: ";
+
+    string svgPartTwo =
+        "; }    .stop2 { stop-color: white; stop-opacity: 0; }    .stop3 { stop-color: yellow; }      </style>  <rect width='100%' height='100%' fill='url(#Gradient1)' />  <text    x='50%'    y='50%'    class='base'    dominant-baseline='middle'    text-anchor='middle'  >";
+
+    string[] colors = [
+        "red",
+        "#08C2A8",
+        "black",
+        "yellow",
+        "blue",
+        "green",
+        "#FFF",
+        "purple",
+        "orange",
+        "brown",
+        "pink",
+        "grey",
+        "cyan"
+    ];
 
     string[] firstWords = [
         "Ferrari",
@@ -70,6 +89,8 @@ contract MyEpicNFT is ERC721URIStorage {
         "Soul"
     ];
 
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
+
     constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("It's my NFT contract baby!");
     }
@@ -120,6 +141,18 @@ contract MyEpicNFT is ERC721URIStorage {
         return thirdWords[rand];
     }
 
+    function pickRandomColor(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("COLOR", Strings.toString(tokenId)))
+        );
+        rand = rand % colors.length;
+        return colors[rand];
+    }
+
     function makeAnEpicNFT() public {
         uint256 newItemId = _tokensIds.current();
 
@@ -129,9 +162,16 @@ contract MyEpicNFT is ERC721URIStorage {
         string memory combinedWord = string(
             abi.encodePacked(first, " - ", second, " - ", third)
         );
+        string memory randomColor = pickRandomColor(newItemId);
 
         string memory finalSvg = string(
-            abi.encodePacked(baseSvg, combinedWord, "</text></svg>")
+            abi.encodePacked(
+                svgPartOne,
+                randomColor,
+                svgPartTwo,
+                combinedWord,
+                "</text></svg>"
+            )
         );
 
         string memory json = Base64.encode(
@@ -176,5 +216,7 @@ contract MyEpicNFT is ERC721URIStorage {
             newItemId,
             msg.sender
         );
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
     }
 }
